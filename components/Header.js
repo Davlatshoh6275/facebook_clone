08234f4e49531f2f2b5b1b915b2a7920248f0089
Davlatshoh <davlatshoh6275@gmail.com>
2023-12-15 15:33:55 +0300

    created header component

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        {/* Left  */}
+        {/* Center  */}
+        {/* Right  */}
+    </div>
+  )
+}
+
+export default Header
